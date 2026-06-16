ae5caa1e371be7ce820de5d28c4bd7f9dfcfce4f
buildaiwithsaurabh <saurabh9453spandits@gmail.com>
2026-06-16 09:14:44 +0530

    Day 25: Implement current user API and role-based authorization

---
+++ backend-fastapi/day-25/database_setup.py
@@ -0,0 +1,28 @@
+import sqlite3
+
+# ==========================================
+# DATABASE SETUP
+# ==========================================
+
+conn = sqlite3.connect("users.db")
+
+cursor = conn.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS users (
+
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL,
+    role TEXT NOT NULL
+)
+""")
+
+conn.commit()
+
+print("Users Table Created Successfully")
+
+conn.close()
+
+print("Database Connection Closed")
